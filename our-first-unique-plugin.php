76a39dc27e615695be1cf9dff4cf6ffc78051403
Daniel Popescu <same.dan@gmail.com>
2023-07-27 10:55:34 +0200

    Settings Page (class) for the Plugin

--- our-first-unique-plugin.php
+++ our-first-unique-plugin.php
@@ -8,11 +8,42 @@
   Author URI: https://articole-smart.eu
 */
 
-add_filter('the_content', 'addToEndOfPost');
+class WordCountAndTimePlugin {
+  // CONSTRUCTOR 
+  function __construct(){ // function when calling a class
+     add_action('admin_menu', array(
+        $this, // this class
+        'adminPage'
+    ));
+  }
+  // END CONSTRUCTOR 
 
-function addToEndOfPost($content) {
-  if(is_single() && is_main_query()) { // just seeing the single post, not without other posts or sidebars
-    return $content . '<p>My Name is Dan.</p>'; 
-  } return $content;
-  
+
+  function adminPage() { 
+   add_options_page(
+    'Word Count Settings',
+    'Word Count', // the title in the settings menu (on the page menu)
+    'manage_options', //for admins
+    'word-count-settings-page', // slug name (url after /wpadmin)
+    array(
+     $this,
+     'ourHTML'
+    )
+     );
+   }
+
+   function ourHTML () {
+     ?>
+       <div class="wrap">
+        <h1>Word Count Settings</h1>
+       </div>
+     <?php
+   }
 }
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
+
+
+
+
+
